800916d2563653ddcf76eca288aff297304e1ebb
Olha LAKTIONOVA <olaktion@e1r11p3.unit.ua>
2019-07-24 17:20:41 +0300

    clean

--- doom_team/src/ft_math/v2_rotate.c
+++
@@ -1,6 +0,0 @@
-#include "ft_math.h"
-
-t_vector		v2_rotate(t_vector v, float acos, float asin)
-{
-	return ((t_vector) {v.x * acos - v.y * asin, v.x * asin + v.y * acos});
-}
--- doom_team/src/system/done.c
+++
@@ -1,8 +0,0 @@
-#include "player_stable.h"
-
-int     done(t_system *system)
-{
-    UnloadData(&system->NumSectors, system->sectors);
-    SDL_Quit();
-    return (0);
-}
